997cc7fa94381d5a24a100c61057066726e38aef
victor <victor@elalemanyo.de>
2023-02-22 10:03:29 +0100

    Add randomButton

--- src/coloris.js
+++ src/coloris.js
@@ -8,7 +8,7 @@
   const ctx = document.createElement('canvas').getContext('2d');
   const currentColor = { r: 0, g: 0, b: 0, h: 0, s: 0, v: 0, a: 1 };
   let container, picker, colorArea, colorAreaDims, colorMarker, colorPreview, colorValue, clearButton,
-      closeButton, hueSlider, hueMarker, alphaSlider, alphaMarker, currentEl, currentFormat, oldColor;
+      closeButton, hueSlider, hueMarker, alphaSlider, alphaMarker, currentEl, currentFormat, oldColor, randomButton;
 
   // Default settings
   const settings = {
@@ -33,6 +33,7 @@
     clearLabel: 'Clear',
     closeButton: false,
     closeLabel: 'Close',
+    randomButton: false,
     onChange: () => undefined,
     a11y: {
       open: 'Open color picker',
@@ -44,7 +45,8 @@
       input: 'Color value field',
       format: 'Color format',
       swatch: 'Color swatch',
-      instruction: 'Saturation and brightness selector. Use up, down, left and right arrow keys to select.'
+      instruction: 'Saturation and brightness selector. Use up, down, left and right arrow keys to select.',
+      random: 'Get a new random color'
     }
   };
 
@@ -212,6 +214,10 @@
             colorValue.setAttribute('aria-label', settings.a11y.input);
             colorArea.setAttribute('aria-label', settings.a11y.instruction);
           }
+          break;
+        case 'randomButton':
+          settings.randomButton = !!options.randomButton;
+          randomButton.style.display = settings.randomButton ? 'block' : 'none';
           break;
         default:
           settings[key] = options[key];
@@ -947,6 +953,12 @@
     `<button type="button" id="clr-clear" class="clr-clear" aria-label="${settings.a11y.clear}">${settings.clearLabel}</button>`+
     '<div id="clr-color-preview" class="clr-preview">'+
       `<button type="button" id="clr-close" class="clr-close" aria-label="${settings.a11y.close}">${settings.closeLabel}</button>`+
+      `<button type="button" id="clr-random" class="clr-random" aria-label="${settings.a11y.random}">`+
+        '<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16">'+
+          '<path fill="currentColor" fill-rule="evenodd" d="M8 2.5a5.487 5.487 0 00-4.131 1.869l1.204 1.204A.25.25 0 014.896 6H1.25A.25.25 0 011 5.75V2.104a.25.25 0 01.427-.177l1.38 1.38A7.001 7.001 0 0114.95 7.16a.75.75 0 11-1.49.178A5.501 5.501 0 008 2.5zM1.705 8.005a.75.75 0 01.834.656 5.501 5.501 0 009.592 2.97l-1.204-1.204a.25.25 0 01.177-.427h3.646a.25.25 0 01.25.25v3.646a.25.25 0 01-.427.177l-1.38-1.38A7.001 7.001 0 011.05 8.84a.75.75 0 01.656-.834z"></path>'+
+          '<path fill="white" fill-rule="evenodd" d="M8 2.5a5.487 5.487 0 00-4.131 1.869l1.204 1.204A.25.25 0 014.896 6H1.25A.25.25 0 011 5.75V2.104a.25.25 0 01.427-.177l1.38 1.38A7.001 7.001 0 0114.95 7.16a.75.75 0 11-1.49.178A5.501 5.501 0 008 2.5zM1.705 8.005a.75.75 0 01.834.656 5.501 5.501 0 009.592 2.97l-1.204-1.204a.25.25 0 01.177-.427h3.646a.25.25 0 01.25.25v3.646a.25.25 0 01-.427.177l-1.38-1.38A7.001 7.001 0 011.05 8.84a.75.75 0 01.656-.834z"></path>'+
+        '</svg>'+
+      '</button>'+
     '</div>'+
     `<span id="clr-open-label" hidden>${settings.a11y.open}</span>`+
     `<span id="clr-swatch-label" hidden>${settings.a11y.swatch}</span>`;
@@ -965,6 +977,7 @@
     hueMarker = getEl('clr-hue-marker');
     alphaSlider = getEl('clr-alpha-slider');
     alphaMarker = getEl('clr-alpha-marker');
+    randomButton = getEl('clr-random');
 
     // Bind the picker to the default selector
     bindFields(settings.el);
@@ -1075,6 +1088,10 @@
     addListener(colorArea, 'click', moveMarker);
     addListener(hueSlider, 'input', setHue);
     addListener(alphaSlider, 'input', setAlpha);
+    addListener(randomButton, 'click', event => {
+      setColorFromStr(`#${(Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6)}`);
+      pickColor();
+    });
   }
 
   /**
